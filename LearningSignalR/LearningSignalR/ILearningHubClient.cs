8e10bbebebcc01da8ba8dd5c6239699659b1e702
subramanian <subramanian.nk@live.com>
2022-08-04 09:26:28 +0530

    signal r demo

---
+++ LearningSignalR/LearningSignalR/ILearningHubClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace LearningSignalR
+{
+    public interface ILearningHubClient
+    {
+        Task ReceiveMessage(string message);
+    }
+}
---
+++ LearningSignalR/LearningSignalR/LearningHub.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace LearningSignalR
+{
+    public class LearningHub : Hub<ILearningHubClient>
+    {
+        public async Task BroadcastMessage(string message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+
+        public async Task SendToCaller(string message)
+        {
+            await Clients.Caller.ReceiveMessage(message);
+        }
+
+        public async Task SendToOthers(string message)
+        {
+            await Clients.Others.ReceiveMessage(message);
+        }
+
+        public async Task SendToGroup(string groupName, string message)
+        {
+            await Clients.Group(groupName).ReceiveMessage(message);
+        }
+
+        public async Task AddUserToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.ReceiveMessage($"Current user added to {groupName} group");
+            await Clients.Others.ReceiveMessage($"User {Context.ConnectionId} added to {groupName} group");
+        }
+
+        public async Task RemoveUserFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.ReceiveMessage($"Current user removed from {groupName} group");
+            await Clients.Others.ReceiveMessage($"User {Context.ConnectionId} removed from {groupName} group");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "HubUsers");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "HubUsers");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
